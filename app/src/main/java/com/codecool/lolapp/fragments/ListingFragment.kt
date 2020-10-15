6614010e3f0d3933df1e9de8944ac04a91ac5a07
Drosztmér István <drosztmer.istvan@gmail.com>
2020-10-15 12:42:32 +0200

    Add dependencies

--- app/src/main/java/com/codecool/lolapp/fragments/ListingFragment.kt
+++ app/src/main/java/com/codecool/lolapp/fragments/ListingFragment.kt
@@ -16,4 +16,9 @@ class ListingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_listing, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.title = "Listing"
+    }
+
 }
