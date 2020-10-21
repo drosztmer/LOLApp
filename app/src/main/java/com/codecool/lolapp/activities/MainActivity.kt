4c2a721d793a5758781edaeba5ce9a1b1e93de71
Drosztmér István <drosztmer.istvan@gmail.com>
2020-10-22 00:44:33 +0200

    Add splash as xml

--- app/src/main/java/com/codecool/lolapp/activities/MainActivity.kt
+++ app/src/main/java/com/codecool/lolapp/activities/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var favouritesFragment: FavouritesFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
