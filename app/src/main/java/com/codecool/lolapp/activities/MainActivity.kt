c09a9faaa865cfcfa36b68e480e49392375f528a
Drosztmér István <drosztmer.istvan@gmail.com>
2020-10-21 14:24:59 +0200

    Add Lore button

--- app/src/main/java/com/codecool/lolapp/activities/MainActivity.kt
+++ app/src/main/java/com/codecool/lolapp/activities/MainActivity.kt
@@ -3,15 +3,11 @@ package com.codecool.lolapp.activities
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.codecool.lolapp.R
-import com.codecool.lolapp.fragments.DetailsFragment
 import com.codecool.lolapp.fragments.FavouritesFragment
 import com.codecool.lolapp.fragments.ListingFragment
-import com.codecool.lolapp.fragments.LoreFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -40,7 +36,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
-        if (id == R.id.favorite) {
+        if (id == R.id.favourite) {
             openFavorites()
             return true;
         }
