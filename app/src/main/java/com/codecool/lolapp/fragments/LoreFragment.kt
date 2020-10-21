e76f4f501d9202b6d88aaa8d3354b4ebeec6538c
Drosztmér István <drosztmer.istvan@gmail.com>
2020-10-21 15:17:29 +0200

    Show lore data

--- app/src/main/java/com/codecool/lolapp/fragments/LoreFragment.kt
+++ app/src/main/java/com/codecool/lolapp/fragments/LoreFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import com.codecool.lolapp.R
 import com.codecool.lolapp.model.Details
 import com.codecool.lolapp.util.DETAILS
+import kotlinx.android.synthetic.main.fragment_lore.*
 
 class LoreFragment : Fragment() {
 
@@ -25,6 +26,13 @@ class LoreFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         activity?.title = getString(R.string.lore_title)
         println(details.lore)
+        showInfo()
+    }
+
+    private fun showInfo() {
+        lore_text.text = details.lore
+        ally_tips_text.text = details.allyTips.joinToString(separator = "\n")
+        enemy_tips_text.text = details.enemyTips.joinToString(separator = "\n")
     }
 
 }
